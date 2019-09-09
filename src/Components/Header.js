258f16c2169c7b76c15fa535929b392d931f9a38
Kwadwo <addoasa94@gmail.com>
2019-09-08 22:40:43 -0400

    Re positioned logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -17,7 +17,8 @@ class Header extends React.Component{
       <div className="header">
         <div className="logo">
           {/* <span id="SVGtitle">SVG</span><span id="tinkertitle">-Tinker</span> */}
-        <img className="logo-img" src= 'https://svgtinkerassets.s3.amazonaws.com/svgtinkerbannerlogolarge.png' />
+        <img className="logo-img" src= 'https://svgtinkerassets.s3.amazonaws.com/svgtinkerbannerlogolargewhite2.png' />
+        <p>Currently in Alpha (version 0.2.0)</p>
         {/* <i class="fas fa-cogs"></i> */}
         </div>
       </div>
