b666820a6627b9a08d08f8b86cfe1baa273d76e2
Kwadwo Asamoah <addoasa94@gmail.com>
2020-06-12 21:21:17 -0400

    Added tests for ADD_VERTICES action for paths in masterReducer.test.js

--- __tests__/masterReducer.test.js
+++ __tests__/masterReducer.test.js
@@ -3,12 +3,7 @@ import {insertIntoMaster, removeFromMaster, useSlider, addVertices, removeVertic
 import * as types from "../actionTypesVariableNames/actionTypeVariables";
 
 describe("Testing Master Reducer actions", ()=>{
-	let mockInsertIntoMaster = insertIntoMaster({},"");
-	const mockInsertIntoMasterAction={
-		type:types.INSERT_INTO_MASTER,
-		payload: mockInsertIntoMaster.payload
-	};
-
+    
 	let defaultState;
 	beforeEach(()=>{
 		defaultState = {
@@ -16,6 +11,12 @@ describe("Testing Master Reducer actions", ()=>{
 		};
 	});
     
+	let mockInsertIntoMaster = insertIntoMaster({},"");
+	const mockInsertIntoMasterAction={
+		type:types.INSERT_INTO_MASTER,
+		payload: mockInsertIntoMaster.payload
+	};
+
 	describe("ACTION: INSERT_INTO_MASTER.", ()=>{
 		it("The INSERT_INTO_MASTER action that the master reducer recieves should add a new SVG to the masterSVGArray",()=>{
 			// Deconstruct masterSVGArray after being altered by the reducer
@@ -28,9 +29,47 @@ describe("Testing Master Reducer actions", ()=>{
 			expect(typeof masterSVGArray[0].svgType).toBe("string");
 		});
 		it("The SVG object being added should have a key called 'svgData' of data type 'object'",()=>{
-            const { masterSVGArray } = masterReducer(defaultState,mockInsertIntoMasterAction);
+			const { masterSVGArray } = masterReducer(defaultState,mockInsertIntoMasterAction);
 			expect(masterSVGArray[0].svgData);
-            expect(typeof masterSVGArray[0].svgData).toBe("object");
+			expect(typeof masterSVGArray[0].svgData).toBe("object");
 		});
+	});
+
+    
+	const mockAddVerticesAction={
+		type:types.ADD_VERTICES,
+		payload: "1"
+	};
+
+	describe("ACTION: ADD_VERTICES.", ()=>{
+		let mockMasterSVGArrayContainingPaths = {
+			masterSVGArray : [
+				{
+					svgType:"PATH",
+					svgData:{},
+				},
+				{
+					svgType:"PATH",
+					svgData:{
+						sliderX0:275,
+						sliderY1:175,
+						sliderX2:225,
+						sliderY3:100,
+						sliderX4:275,
+						sliderY5:200,
+					},
+				},
+			]
+        };
+        
+		it("If an SVG object of the 'svgType': 'PATH' is added to the masterSVGArray, a new pair of slider key value pairs should be added to that path svg object's 'svgData' object",()=>{
+			// Deconstruct masterSVGArray after being altered by the reducer
+			const { masterSVGArray } = masterReducer(mockMasterSVGArrayContainingPaths,mockAddVerticesAction);
+            expect(masterSVGArray[1].svgData.hasOwnProperty("sliderX6"));
+            expect(masterSVGArray[1].svgData.hasOwnProperty("sliderY7"));
+            console.log(masterSVGArray[1].svgData.hasOwnProperty("sliderX6"));
+            console.log(masterSVGArray[1].svgData.hasOwnProperty("sliderY7"));
+		});
+		
 	});
 });
