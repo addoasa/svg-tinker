d7e304743640fa1b5de5515559155c71588b3bed
Kwadwo Asamoah <addoasa94@gmail.com>
2020-07-25 19:06:31 -0400

    Updated to version 0.1.15

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,7 +9,7 @@ class Header extends React.Component{
     return(
       <div className="header">
         <div>
-          <span>SVG TINKER v 0.1.14
+          <span>SVG TINKER v 0.1.15
             <a className= "github-link" href="https://github.com/addoasa/svg-tinker">
               <i className="fab fa-github"></i>
             </a>
